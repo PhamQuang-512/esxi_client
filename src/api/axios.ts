19bd2b8f07f1c11775b5b0e55132b0c1eba73ad6
blueslimeduong <blueslimeduong@gmail.com>
2022-12-04 17:02:32 +0700

    fix api url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const isAxiosError = axios.isAxiosError;
 
 const api = axios.create({
-    baseURL: '//localhost:8080/api',
+    baseURL: '//192.168.1.2:8080/api',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
